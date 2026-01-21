3d501bbabcb96549ef3621bbdb6807b2fbe5ffa3
Lucas Eliason <lukeeli2005@gmail.com>
2026-01-21 12:44:21 -0500

    Initial Commit

---
+++ InventorySystem/ClassInteraction/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInteraction
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsObtained { get; set; }
+
+        public enum ItemCount
+        {
+            Money,
+            ItemCount,
+
+        }
+    }
+}

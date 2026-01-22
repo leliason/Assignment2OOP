5c0be0da4e48f9e4005541a8ee6ca3fef43be5a6
Lucas Eliason <lukeeli2005@gmail.com>
2026-01-22 01:58:26 -0500

    Progress Class1 File

--- InventorySystem/ClassInteraction/Class1.cs
+++ InventorySystem/ClassInteraction/Class1.cs
@@ -9,16 +9,19 @@ namespace ClassInteraction
 {
     public class Item
     {
-        public int ItemId { get; set; }
+        public int ItemId { get; private set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public bool IsObtained { get; set; }
 
-        public enum ItemCount
+        public enum ItemCategory
         {
-            Money,
-            ItemCount,
-
+            Weapon,
+            Consumable,
+            Currency
         }
+        public ItemCategory Category { get; private set; }
+        private List<String> ItemHistory;
+
     }
 }
+

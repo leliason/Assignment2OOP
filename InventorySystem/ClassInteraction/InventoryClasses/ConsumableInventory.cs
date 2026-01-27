3ad4e508ff4ecbfa45e2404a740e83b70c88c575
Lucas Eliason <lukeeli2005@gmail.com>
2026-01-27 12:38:24 -0500

    Create ConsumableInventory.cs

---
+++ InventorySystem/ClassInteraction/InventoryClasses/ConsumableInventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInteraction.InventoryClasses
+{
+    public class ConsumableInventory : Inventory
+    {
+        public int UsesRemaining {  get; private set; }
+
+        public ConsumableInventory(int ItemID, string name, int slots, int uses) 
+            : base(ItemID, name, ItemCategory.Consumable, slots)
+        {
+            UsesRemaining = uses;
+        }
+
+        public override bool RemoveItem(string itemname, int quantity)
+        {
+            if (quantity <= 0 || UsesRemaining <= 0)
+            {
+                return false;
+            }
+            UsesRemaining -= quantity;
+            return true;
+        }
+
+
+
+
+    }
+}
